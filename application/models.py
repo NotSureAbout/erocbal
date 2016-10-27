4cf52ee9b9da9827e3f6eb1c1e77bd47c679a439
Oliver Marx <om@poplar.dk>
2016-10-27 15:42:37 +0200

    UTF-8 issues part 2.

--- application/models.py
+++ application/models.py
@@ -19,7 +19,13 @@ class User(db.Model):
 
     @staticmethod
     def hashed_password(password):
-        return bcrypt.generate_password_hash(password)
+        try:
+            hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        except AttributeError:
+            hashed_pw = bcrypt.generate_password_hash(password)
+        except Exception as e:
+            raise e
+        return hashed_pw
 
     @staticmethod
     def get_user_with_email_and_password(email, password):
